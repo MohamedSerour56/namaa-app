f9450a743e0c7baf2fef693314de71d27f97ee03
MohamedSerour56 <mohamedserour56@gmail.com>
2026-09-26 11:13:05 -0700

    Build with GitHub Pages asset paths

---
+++ _next/static/PKL7ParTHihpuYO56UR7-/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/namaa-app//_next/:path+",
+        "destination": "/namaa-app/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
